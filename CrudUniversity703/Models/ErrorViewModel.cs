589d969321b91934df4300b6c3f24ff8c89a36ee
jmora05 <juanijo3716@gmail.com>
2025-10-04 20:08:52 -0500

    a
    ascd

--- CrudUniversity703/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrudUniversity703.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
